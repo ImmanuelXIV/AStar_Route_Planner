298007ff943537f1f4e8b542fe3fe563f68a6e5f
ImmanuelXIV <manuel.schwall@gmail.com>
2020-08-10 10:38:15 +0200

    feat: improve performance in route_planner via emplace_back and changing the lamda function

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -11,39 +11,33 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-  	start_node = &m_Model.FindClosestNode(start_x, start_y);
-  	end_node = &m_Model.FindClosestNode(end_x, end_y);
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-  	return node->distance((*end_node));
+    return node->distance((*end_node));
 }
 
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
-  	current_node->FindNeighbors();
-    for (RouteModel::Node * n : current_node->neighbors){
-      	if (n->visited == false){  
+    current_node->FindNeighbors();
+    for (auto* n : current_node->neighbors){
+        if (n->visited == false){  
             n->parent = current_node;
             n->h_value = CalculateHValue(n);
-          	n->g_value = current_node->g_value + current_node->distance(*n); // g + d;
-            open_list.push_back(n);
+            n->g_value = current_node->g_value + current_node->distance(*n); // g + d;
             n->visited = true;
+            open_list.emplace_back(n);
         }
- 	}
+  }
 }
 
 
 RouteModel::Node *RoutePlanner::NextNode() {
-    // This is a lambda function
-    auto Compare = [](RouteModel::Node* a, RouteModel::Node* b){
-        float f1 = a->h_value + a->g_value;
-        float f2 = b->h_value + b->g_value;
-        return f1 > f2;
-    };
-    // sort in descending order
-    std::sort(open_list.begin(), open_list.end(), Compare);
+    // sort in descending order via lambda function
+    std::sort(open_list.begin(), open_list.end(), [] (const RouteModel::Node* a, const RouteModel::Node* b) {return a->g_value + a->h_value > b->g_value + b->h_value;});
     RouteModel::Node* lowest_node = open_list.back();
     open_list.pop_back();
     return lowest_node;
@@ -56,12 +50,12 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
-  	while ((*current_node).parent != nullptr){
-      	distance += current_node->distance((*(*current_node).parent));
-      	path_found.insert(path_found.begin(), (*current_node));
-      	current_node = (*current_node).parent;
-    }	
-  	path_found.insert(path_found.begin(), (*current_node));
+    while ((*current_node).parent != nullptr){
+        distance += current_node->distance((*(*current_node).parent));
+        path_found.insert(path_found.begin(), (*current_node));
+        current_node = (*current_node).parent;
+    } 
+    path_found.insert(path_found.begin(), (*current_node));
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
 }
@@ -71,17 +65,17 @@ void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
     // TODO: Implement your solution here.
-  	start_node->visited = true;
-  	start_node->h_value = CalculateHValue(start_node);
+    start_node->visited = true;
+    start_node->h_value = CalculateHValue(start_node);
     start_node->g_value = 0;
     AddNeighbors(start_node); 
   
-  	while (open_list.size() > 0) {
-      	current_node = NextNode();
-      	if (current_node->x == end_node->x && current_node->y == end_node->y){
-          	m_Model.path = ConstructFinalPath(current_node);
-          	return;
+    while (open_list.size() > 0) {
+        current_node = NextNode();
+        if (current_node == end_node){
+            m_Model.path = ConstructFinalPath(current_node);
+            return;
         }
-      	AddNeighbors(current_node);
+        AddNeighbors(current_node);
     }
 }
